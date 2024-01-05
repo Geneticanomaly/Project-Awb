65df584f1617d6b282a95077cf221e2c70cf6544
Geneticanomaly <thomaskaatranen@gmail.com>
2024-01-05 14:20:31 +0200

    Fixed Navbar styling issues caused by previous commit.

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,17 +1,12 @@
 import logo_color from '../../images/color-logo-tinder.png';
 import './Navbar.css';
 
-type NavProps = {
-    authToken: boolean;
-};
-
-function Navbar({authToken}: NavProps) {
+function Navbar() {
     return (
         <nav className="nav">
             <div className="logo-container">
                 <img className="logo" src={logo_color} />
             </div>
-            {authToken && <img className="profile-img" src="https://i.imgur.com/oPj4A8u.jpeg" />}
         </nav>
     );
 }
